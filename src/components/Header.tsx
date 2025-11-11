1861e6e8d2be2eab6f78811c25389d10c5304501
Ronald De Mesa Jr. <ronaldreyjr061201@gmail.com>
2025-11-11 14:38:33 +0800

    replace: header color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,21 +4,36 @@ import { Button } from "./ui/button";
 
 function Header() {
   return (
-    <header className="w-screen h-[100px] flex flex-row justify-between align-center">
+    <header className="w-screen h-[50px] flex flex-row items-end bg-red-100">
       <ul className="w-full flex flex-row md:gap-3 items-center justify-center">
         <li>
           <Button asChild>
-            <Link to={"/"}>Home</Link>
+            <Link
+              to={"/"}
+              className="hover:underline transition ease-in-out duration-15"
+            >
+              Home
+            </Link>
           </Button>
         </li>
         <li>
           <Button asChild>
-            <Link to={"/products"}>Products</Link>
+            <Link
+              to={"/products"}
+              className="hover:underline transition ease-in-out duration-15"
+            >
+              Products
+            </Link>
           </Button>
         </li>
         <li>
           <Button asChild>
-            <Link to={"/about"}>About</Link>
+            <Link
+              to={"/about"}
+              className="hover:underline transition ease-in-out duration-15"
+            >
+              About
+            </Link>
           </Button>
         </li>
       </ul>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,14 +1,19 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { Link } from "react-router";
+import pfp from "../../public/pfp.jpg";
 
 function Landing() {
   return (
     <div>
       <div className="flex gap-2 flex-row-reverse flex-wrap items-center justify-center lg:flex-nowrap">
-        <div className="min-w-[220px] min-h-[300px]">image</div>
-        <div>
-          <span className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+        <img
+          src={pfp}
+          alt="person with red coat"
+          className="aspect-square min-w-[220px] min-h-[220px] max-w-[320px] object-fill border border-white rounded-full"
+        />
+        <div className="flex flex-col items-center justify-center lg:justify-start">
+          <span className=" scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
             Protect Your Future with Smart Insurance & Financial Planning
           </span>
           <p>
@@ -26,7 +31,7 @@ function Landing() {
         <Link to={"/"}>Get a Personalized Quote</Link>
         {/* {create thing for the link} */}
       </Button>
-
+      <div></div>
       <div className="mt-40">
         images here, maybe we can do 2 col to 1 col or 1 3 1 or maybe 3 1 or
         maybe 1 3
